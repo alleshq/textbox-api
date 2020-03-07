6a3e8fb5166a2f3e66bf45b4b176a97ec38f4eab
Archie Baer <archie@abaer.dev>
2020-03-07 20:21:49 +0000

    Ratelimiting

--- api/v1/new.js
+++ api/v1/new.js
@@ -1,4 +1,5 @@
 const db = require("../../util/db");
+const Sequelize = require("sequelize");
 const uuid = require("uuid/v4");
 const randomString = require("randomstring").generate;
 
@@ -7,6 +8,18 @@ module.exports = async (req, res) => {
     if (typeof req.body.title !== "string" || typeof req.body.content !== "string" || typeof req.body.markdown !== "boolean" || typeof req.body.highlight !== "boolean") return res.status(400).json({err: "invalidBodyParameters"});
     if (req.body.title.trim().length < 3 || req.body.title.length > 100) return res.status(400).json({err: "titleLength"});
     if (req.body.content.trim().length < 10 || req.body.content.length > 10000) return res.status(400).json({err: "contentLength"});
+
+    //Ratelimiting
+    const since = new Date(new Date().getTime() - 1000 * 60);
+    const recentDocs = await db.Document.count({
+        where: {
+            user: req.user.id,
+            createdAt: {
+                [Sequelize.Op.gte]: since
+            }
+        }
+    });
+    if (recentDocs.length > 5) return res.status(429).json({err: "cooldown"});
 
     //Create Document
     const doc = await db.Document.create({
